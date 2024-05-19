fcc94338d44d36fe938605868caa731421d6e16a
sentic90 <princeali5549@gmail.com>
2024-05-19 12:09:31 +0300

    added dynamic classnames + active link navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import Link from 'next/link'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames'
+import { usePathname } from 'next/navigation';
 const NavBar = () => {
-
+    const currentPath = usePathname()
     const links = [
         { label: 'Dashboard',  href:'/'},
         { label: 'Issues',  href:'/issues'},
@@ -12,8 +16,14 @@ const NavBar = () => {
         <Link href='/'><AiFillBug /></Link>
         <ul className='flex space-x-6'>
             {links.map(link => 
-            <Link 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            <Link  key={link.href}
+            className={
+                classnames({
+                    'text-zinc-900':currentPath === link.href,
+                    'text-zinc-500':currentPath !== link.href,
+                    'hover:text-zinc-800 transition-colors':true
+                })
+            }
             href={link.href}>{link.label}</Link>)
             }
         </ul>
