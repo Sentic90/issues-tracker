e49d7b0bab6cb4a1a46c78f6298aecf86a30c1b8
sentic90 <princeali5549@gmail.com>
2024-05-24 15:05:53 +0300

    added loading skeleton

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,14 +3,17 @@ import Link from 'next/link'
 import React from 'react'
 import prisma from "@/prisma/client";
 import IssueStatusBadge from '../components/IssueStatusBadge';
+import delay from 'delay'
+import IssuesAction from './IssuesAction';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany()
+
+
+  await delay(2000)
   return (
     <div>
-      <div className='mb-5'>
-      <Button><Link href="/issues/new">New Issue</Link></Button>
-      </div>
+      <IssuesAction />
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
