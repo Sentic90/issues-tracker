248c52b4edf88c092ec20a3d1286f107b6385c9d
sentic90 <princeali5549@gmail.com>
2024-05-25 16:02:14 +0300

    add additional loading skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Box, Card, Flex } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingDetailsPage = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <Box>
+      <Skeleton className="max-w-xl"/>
+      <Flex className="space-x-4" my="3">
+        <Skeleton width="4rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={4} />
+      </Card>
+    </Box>
+  );
+};
 
-export default LoadingDetailsPage
+export default LoadingDetailsPage;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,7 +3,7 @@ import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
-
+import delay from 'delay'
 interface Props {
   params: { id: string };
 }
@@ -15,6 +15,8 @@ const IssueDetailsPage = async ({ params }: Props) => {
   });
 
   if (!issue) notFound();
+
+  await delay(2000);
   return (
     <div>
       <Heading>{issue?.title}</Heading>
