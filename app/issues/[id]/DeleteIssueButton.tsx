0c2c53c723bab4258e4d07941c071bcf7b6bdbc8
sentic90 <princeali5549@gmail.com>
2024-05-28 10:38:12 +0300

    showing confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { FaTrash } from "react-icons/fa";
@@ -8,7 +8,29 @@ import { FaTrash } from "react-icons/fa";
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   return (
-    <Button
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <FaTrash />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure of delete this issue?, this operations not undone.
+        </AlertDialog.Description>
+        <Flex mt='4' gap="3">
+          <AlertDialog.Action>
+            <Button color="red">Confirm Delete</Button>
+          </AlertDialog.Action>
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">Cancel</Button>
+          </AlertDialog.Cancel>
+        </Flex>
+      </AlertDialog.Content>
+
+      {/* <Button
     color="red"
       onClick={async () => {
         // sending HTTP DELETE request
@@ -17,9 +39,9 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
         router.refresh();
       }}
     >
-      <FaTrash />
-      Delete Issue
-    </Button>
+      
+    </Button> */}
+    </AlertDialog.Root>
   );
 };
 
