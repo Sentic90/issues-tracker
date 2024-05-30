51d75c202145fe20963307d399f090675146b260
sentic90 <princeali5549@gmail.com>
2024-05-30 21:59:28 +0300

    disentgreate navbar in multiple component,

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers () {
+        return [
+            {   // all endpoints in app
+                source:"/:path*",
+                headers:[
+                    { key: 'referrer-policy', value:'no-referrer'}
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
