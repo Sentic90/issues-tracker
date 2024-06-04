986fbd7e2bb0da96de177cf222342b9dba6c4813
sentic90 <princeali5549@gmail.com>
2024-06-04 13:38:46 +0300

    added toast notification, AssignedUser

--- app/issues/[id]/AssigneeUser.tsx
+++ app/issues/[id]/AssigneeUser.tsx
@@ -1,37 +1,62 @@
 "use client";
 
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Skeleton } from '@/app/components'
+import { Skeleton } from "@/app/components";
+import toast, { Toaster } from "react-hot-toast";
 
+const AssigneUser = ({ issue }: { issue: Issue }) => {
+  const { data: users, error, isLoading } = useUsers();
 
-const AssigneUser = () => {
+  const assignUser = async (userId: string) => {
+    const value = userId === "none" ? null : userId;
 
-  const {data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get("/api/users").then( res => res.data),
-    staleTime: 1000 * 60,
-    retry: 3,
-  })
+    try {
+      const res = await axios.patch("/api/issues/" + issue.id, {
+        assignedToUserId: value,
+      });
 
+      if (res.status === 200) {
+        toast.success("Issue successfully Assigned");
+      }
+    } catch (error) {
+      toast.error("Changes could not be saved.");
+    }
+  };
 
-  if (isLoading) return <Skeleton height='2rem'/>
-  
+  if (isLoading) return <Skeleton height="2rem" />;
+
   if (error) return null;
   return (
-    <Select.Root>
-      <Select.Trigger placeholder="Assgining to..." />
-      <Select.Content>
-        <Select.Group>
-          { users?.map(user => (
-            <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || "none"}
+        onValueChange={assignUser}
+      >
+        <Select.Trigger placeholder="Assgining to..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Item value="none">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 1000 * 60,
+    retry: 3,
+  });
 export default AssigneUser;
