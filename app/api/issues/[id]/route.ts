e43a3084d1cdfe6699c37f2a2172769a73edbaa2
sentic90 <princeali5549@gmail.com>
2024-06-04 10:51:39 +0300

    added AssignedToUserId schema, improve API to handle userId

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 // import schema from "../schema";
 import prisma from "@/prisma/client";
-import {IssueSchema} from '@/app/validationSchemas'
+import {patchIssueSchema} from '@/app/validationSchemas'
 import delay from "delay";
 import authOptions from "@/app/auth/AuthOptions";
 import { getServerSession } from "next-auth";
@@ -26,27 +26,36 @@ export async function PATCH(request: NextRequest, { params }: Props) {
   // validate the request body
   const body = await request.json();
 
-  const session = await getServerSession(authOptions)
-  if (!session) return NextResponse.json({error: "Unauthroized"}, {status:401})
+  // const session = await getServerSession(authOptions)
+  // if (!session) return NextResponse.json({error: "Unauthroized"}, {status:401})
 
 
-  const validation = IssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
-  const user = await prisma.issue.findUnique({
+  const { assignedToUserId, title, description } = body;
+
+  if (assignedToUserId){
+    const user = await prisma.user.findUnique({ where: { id: assignedToUserId}})
+    if(!user){
+      return NextResponse.json({error: 'User not exist'}, { status: 400})
+    }
+  }
+  const issue = await prisma.issue.findUnique({
     where: {
       id: parseInt(params.id),
     },
   });
-  if (!user)
+  if (!issue)
     return NextResponse.json({ error: "Issue Not Found" }, { status: 404 });
 
   const updatedIssue = await prisma.issue.update({
-    where: { id: user.id },
+    where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId
     },
   });
   return NextResponse.json(updatedIssue, { status: 200 });
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,7 +1,16 @@
-import { z } from 'zod'
-
+import { z } from "zod";
 
 export const IssueSchema = z.object({
-    title: z.string().min(1, "This Field Required").max(255),
-    description: z.string().min(1, "This Field Required"),
+  title: z.string().min(1, "This Field Required").max(255),
+  description: z.string().min(1, "This Field Required"),
+});
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "This Field Required").max(255).max(255).optional(),
+  description: z.string().min(1, "This Field Required").max(6666).optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId required.")
+    .max(255)
+    .optional()
+    .nullable(),
 });
