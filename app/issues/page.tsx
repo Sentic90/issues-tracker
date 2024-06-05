d225c0e28343d27bc048ff496cb637b925081c05
sentic90 <princeali5549@gmail.com>
2024-06-05 08:41:30 +0300

    sort issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -10,14 +10,7 @@ interface Props {
   searchParams: { status: Status; orderBy: keyof Issue };
 }
 const IssuesPage = async ({ searchParams }: Props) => {
-  const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
-  const issues = await prisma.issue.findMany({
-    where: { status },
-  });
-
+  // Sorting
   const columns: {
     label: string;
     value: keyof Issue;
@@ -27,6 +20,25 @@ const IssuesPage = async ({ searchParams }: Props) => {
     { label: "Status", value: "status" },
     { label: "Created At", value: "createdAt" },
   ];
+
+  // Filtering
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  // Compute orderBy value
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status },
+    orderBy,
+  });
+
   return (
     <Box>
       <IssuesAction />
