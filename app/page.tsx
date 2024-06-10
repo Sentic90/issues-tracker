196a626be5fd1cea7a765e951097626cca7559de
sentic90 <princeali5549@gmail.com>
2024-06-10 14:02:00 +0300

    Build Summary Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import IssuesSummary from "./IssuesSummary";
 import LatestIssues from "./LatestIssues";
 
 export default function Home() {
-  return <LatestIssues />;
+  // <LatestIssues />;
+  return <IssuesSummary open={100} closed={2121} inProgress={18} />;
 }
